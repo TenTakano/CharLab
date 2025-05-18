ec95a98cec2b0c30897721d4400524659166f5b2
TenTakano <takano20771@gmail.com>
2025-05-19 01:22:58 +0900

    Change Directory Structure

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/main.ts",
+  entry: "./src/main/main.ts",
   target: "electron-main",
   mode: process.env.NODE_ENV || "development",
   output: {
