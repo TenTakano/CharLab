f58fd1e8e304c1725c027d7c79f6a96d5e271b99
TenTakano <takano20771@gmail.com>
2025-05-28 02:34:29 +0900

    Integrate Asset to React

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
+import noImage from "./assets/noimage.svg";
 
 const App: FC = () => {
   return (
     <div>
       <h1>Welcome to the App</h1>
       <p>This is a simple React application.</p>
+      <img src={noImage} />
     </div>
   );
 };
