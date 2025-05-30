50d72784597926be639117fcc28875c5d8ab295a
TenTakano <takano20771@gmail.com>
2025-05-31 02:27:06 +0900

    Refactoring

--- src/renderer/components/ContextMenu/index.tsx
+++ src/renderer/components/ContextMenu/index.tsx
@@ -1,5 +1,25 @@
 import type { FC } from "react";
 
+type ContextMenuButtonProps = {
+	onClick: () => void;
+	children: React.ReactNode;
+};
+
+const ContextMenuButton: FC<ContextMenuButtonProps> = ({
+	onClick,
+	children,
+}) => {
+	return (
+		<button
+			type="button"
+			className="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
+};
+
 type Props = {
 	show: boolean;
 	position: { x: number; y: number };
@@ -11,30 +31,10 @@ const ContextMenu: FC<Props> = ({ show, position }) => {
 			className={`${show ? "block" : "hidden"} absolute bg-white dark:bg-gray-800 shadow-lg rounded-md ring-1 ring-black ring-opacity-5 w-48 py-2`}
 			style={{ left: position.x, top: position.y }}
 		>
-			<button
-				type="button"
-				className="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
-			>
-				Refresh Widget
-			</button>
-			<button
-				type="button"
-				className="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
-			>
-				Resize Widget
-			</button>
-			<button
-				type="button"
-				className="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
-			>
-				Change Theme
-			</button>
-			<button
-				type="button"
-				className="w-full text-left px-4 py-2 text-gray-700 dark:text-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700"
-			>
-				Widget Settings
-			</button>
+			<ContextMenuButton onClick={() => {}}>Refresh Widget</ContextMenuButton>
+			<ContextMenuButton onClick={() => {}}>Resize Widget</ContextMenuButton>
+			<ContextMenuButton onClick={() => {}}>Change Theme</ContextMenuButton>
+			<ContextMenuButton onClick={() => {}}>Widget Settings</ContextMenuButton>
 		</div>
 	);
 };
